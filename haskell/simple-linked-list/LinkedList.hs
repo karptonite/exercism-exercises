ffc1ebc45497384c9fd1bd9a52028804d34298a6
Daniel Karp <danielkarp@gmail.com>
2015-07-03 12:12:01 -0400

    Finished first round of Haskell linked list exercise

--- haskell/simple-linked-list/LinkedList.hs
+++ haskell/simple-linked-list/LinkedList.hs
@@ -1,12 +1,33 @@
-module LinkedList (nil, isNil, new)
+module LinkedList (nil, isNil, new, datum, next, toList, fromList, reverseLinkedList)
 where
   
-data List = Alist
+data List a = Empty | Elem a (List a)
 
-nil = Alist
+nil :: List a
+nil = Empty
 
-isNil :: List -> Bool
+isNil :: List a -> Bool
 isNil _ = True
 
-new :: a -> List -> List
-new _  _ = nil
+new :: a -> List a -> List a
+new    = Elem 
+
+datum :: List a -> a
+datum (Elem x _) = x
+
+next :: List a -> List a
+next (Elem _ l) = l
+
+toList :: List a -> [a]
+toList Empty = []
+toList (Elem x l) = x : toList l
+
+fromList :: [a] -> List a
+fromList = foldr Elem Empty
+
+reverseLinkedList :: List a -> List a
+reverseLinkedList Empty = Empty
+reverseLinkedList (Elem x Empty) = Elem x Empty
+reverseLinkedList (Elem x l) = new (datum (reverseLinkedList l)) $ reverseLinkedList $ new x $ reverseLinkedList $ next $ reverseLinkedList l
+
+
