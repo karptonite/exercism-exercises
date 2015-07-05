6b2b98d14e9d5661473a98afdd84b4a2a3460a1d
Daniel Karp <danielkarp@gmail.com>
2015-07-05 14:52:47 -0400

    Improvement to Haskell grade school exercise

--- haskell/grade-school/School.hs
+++ haskell/grade-school/School.hs
@@ -1,27 +1,25 @@
 module School (School, add, sorted, grade, empty)
 where
-import Data.List(groupBy, sortBy)
+import Data.List(insert)
+import qualified Data.Map as Map
 
 type StudentName = String
 type GradeLevel = Int
-type Student = (GradeLevel, StudentName)
 type Grade = ( GradeLevel, [StudentName] )
-type School = [Student]
-type SortedSchool = [Grade]
+type School = Map.Map GradeLevel [StudentName]
 
 empty :: School
-empty = []
+empty = Map.empty
 
 add :: GradeLevel -> StudentName -> School -> School
-add g s sch = (g, s) : sch
+add g s = Map.insertWith (\(a:_) b -> insert a b) g [s]
 
-sorted :: School -> SortedSchool
-sorted sc = map collapseGrade $ groupBy (\(x, _) (y, _) -> x == y) $ sortBy compareStudents sc
-  where
-    collapseGrade :: [Student] -> Grade 
-    collapseGrade  = foldr (\(g, s) (_,ss) -> (g, s:ss) ) (0, [])
-    compareStudents :: Student -> Student -> Ordering
-    compareStudents (g1, s1) (g2, s2) = (g1 `compare` g2) `mappend` (s1 `compare` s2)
+sorted :: School -> [Grade]
+sorted = Map.toList
 
 grade :: GradeLevel -> School -> [StudentName]
-grade g' = foldl (\acc (g, ss) -> if g == g' then ss else acc ) [] . sorted
+grade g sch = extractList $ Map.lookup g sch
+  where 
+    extractList :: Maybe [a] -> [a]
+    extractList Nothing = []
+    extractList (Just xs) = xs
